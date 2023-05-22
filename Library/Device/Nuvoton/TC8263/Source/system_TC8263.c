a06cee202a2c9bbf97fb6675aeb0f883bb7b058d
klchiu0 <klchiu0@nuvoton.com>
2023-05-22 13:21:18 +0800

    Disable cache   Enable cache has side effect for ICE download to SRAM (To be check)

--- Library/Device/Nuvoton/TC8263/Source/system_TC8263.c
+++ Library/Device/Nuvoton/TC8263/Source/system_TC8263.c
@@ -126,11 +126,11 @@ __WEAK void InitDebugUart(void)
 __attribute__ ((constructor)) void SystemInit(void)
 {
 #ifndef NVT_ICACHE_OFF
-    SCB_EnableICache();
+    //SCB_EnableICache();
 #endif
     
 #ifndef NVT_DCACHE_OFF
-    SCB_EnableDCache();
+    //SCB_EnableDCache();
 #endif
 
 #if defined (__VTOR_PRESENT) && (__VTOR_PRESENT == 1U)
