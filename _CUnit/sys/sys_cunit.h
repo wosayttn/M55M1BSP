faed399d4b08f1681d01254bd58ac6874d2cb11a
mcchang2 <mcchang2@nuvoton.com>
2023-06-05 19:12:34 +0800

    Added SYS CUnit test code

---
+++ _CUnit/sys/sys_cunit.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+* @file    sys_cunit.h
+* @version V1.00
+* @brief   CUnit test sys
+*
+* SPDX-License-Identifier: Apache-2.0
+* @copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#ifndef __SYS_CUNIT_H__
+#define __SYS_CUNIT_H__
+
+extern CU_SuiteInfo sysSuites[];
+extern CU_TestInfo  sys_ConstTest[];
+extern CU_TestInfo  sys_MacroTest[];
+extern CU_TestInfo  sys_FuncTest[];
+
+#endif  // __SYS_CUNIT_H__
