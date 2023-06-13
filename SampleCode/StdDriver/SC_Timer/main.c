ef76a64f3feca7999a4a85d6942b7116822028ba
MS70 CYHsu11 <CYHSU11@nuvoton.com>
2023-06-13 10:08:27 +0800

    update _CUnit for clk.c update and upload RTC/SC/SCUART Sample code
    Change-Id: Ib673faf5c636b53f1a5242189d36a75ea0ad06c5

---
+++ SampleCode/StdDriver/SC_Timer/main.c
@@ -0,0 +1,127 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Demonstrate how to use SC embedded timer.
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2021 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Functions declaration                                                                                   */
+/*---------------------------------------------------------------------------------------------------------*/
+void SYS_Init(void);
+void UART_Init(void);
+
+
+/**
+  * @brief  The interrupt services routine of smartcard port 0
+  * @param  None
+  * @retval None
+  */
+void SC0_IRQHandler(void)
+{
+    static uint32_t u32Sec = 1;
+
+    if(SC0->INTSTS & SC_INTSTS_TMR0IF_Msk)
+    {
+        /* Clear interrupt flag */
+        SC0->INTSTS = SC_INTSTS_TMR0IF_Msk;
+        printf("%d sec\n", u32Sec++);
+    }
+}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/    
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Enable PLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
+
+    /* Switch SCLK clock source to PLL0 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+    
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK1DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+    
+    /* Enable SC0 module clock and clock source from HIRC divide 1 */
+    CLK_SetModuleClock(SC0_MODULE, CLK_SCSEL_SC0SEL_HIRC, CLK_SCDIV_SC0DIV(1));
+   
+    /* Enable module clock */
+    CLK_EnableModuleClock(SC0_MODULE);
+
+    /* Enable UART0 module clock */
+    SetDebugUartCLK();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    SetDebugUartMFP();
+        
+}
+
+void UART_Init(void)
+{
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(DEBUG_PORT, 115200);
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int main(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART for printf */
+    UART_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+
+    printf("\nThis sample code demo how to use SC embedded timer.\n");
+
+    /* Open smartcard interface */
+    SC_Open(SC0, SC_PIN_STATE_IGNORE, SC_PIN_STATE_HIGH);
+
+    /* Enable SC TIMER0 interrupt */
+    SC_ENABLE_INT(SC0, SC_INTEN_TMR0IEN_Msk);
+    NVIC_EnableIRQ(SC0_IRQn);
+
+    /* Real ETU divider value is "11+1=12", and the duration of each ETU is 12/SC_CLK */
+    SC0->ETUCTL = 11;
+
+    /* Each 1,000,000 ETU will generate TIMER0 timeout event */
+    SC_StartTimer(SC0, 0, SC_TMR_MODE_4, 1000000);  // timer counter will be reloaded.
+
+    while(1) {}
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
