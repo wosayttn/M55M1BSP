5f6f7dbc07380e1e87bfdd2ff75aadb7d19417a7
mcchang2 <mcchang2@nuvoton.com>
2023-06-19 17:47:38 +0800

    Update AWF for review issue

--- Library/StdDriver/inc/awf.h
+++ Library/StdDriver/inc/awf.h
@@ -15,6 +15,49 @@ extern "C"
 {
 #endif
 
+/*---------------------------------------------------------------------------------------------------------*/
+/*  AWF constant definitions.                                                                              */
+/*---------------------------------------------------------------------------------------------------------*/
+#define AWF_HTINT_ENABLE        (AWF_CTL_HTIEN_Msk)
+#define AWF_LTINT_ENABLE        (AWF_CTL_LTIEN_Msk)
+#define AWF_BOTHINT_ENABLE      (AWF_CTL_HTIEN_Msk | AWF_CTL_LTIEN_Msk)
+#define AWF_BOTHINT_DISABLE     (0x0UL)
+#define AWF_HTWK_ENABLE         (AWF_CTL_HTWKEN_Msk)
+#define AWF_LTWK_ENABLE         (AWF_CTL_LTWKEN_Msk)
+#define AWF_BOTHWK_ENABLE       (AWF_CTL_HTWKEN_Msk | AWF_CTL_LTWKEN_Msk)
+#define AWF_BOTHWK_DISABLE      (0x0UL)
+#define AWF_CLOSE               (0x0UL)
+
+
+/**
+  * @brief      This macro is used to set accumlation count.
+  * @param[in]  u32Count is AccumlationCount. It could be 2~8.
+  */
+#define AWF_SET_ACC_COUNT(u32Count)     (AWF->CTL = (AWF->CTL & ~AWF_CTL_ACUCNT_Msk) | (u32Count << AWF_CTL_ACUCNT_Pos))
+
+/**
+  * @brief      This macro is used to set high threshold value.
+  * @param[in]  u32Value is HTH Value. It could be 0~524288.
+  */
+#define AWF_SET_HTH_VALUE(u32Value)        (AWF->HTH = (AWF->HTH & ~AWF_HTH_AWFHTH_Msk) | (u32Value << AWF_HTH_AWFHTH_Pos))
+
+/**
+  * @brief      This macro is used to set low threshold value.
+  * @param[in]  u32Value is LTH Value. It could be 0~524288.
+  */
+#define AWF_SET_LTH_VALUE(u32Value)        (AWF->LTH = (AWF->LTH & ~AWF_LTH_AWFLTH_Msk) | (u32Value << AWF_LTH_AWFLTH_Pos))
+
+/**
+  * @brief      This macro is used to set AWF word buffer initial value.
+  * @param[in]  u32Value is WBINIT Value. It could be 0~65535.
+  */
+#define AWF_SET_WBINIT_VALUE(u32Value)      (AWF->WBINIT = (AWF->WBINIT & ~AWF_WBINIT_WBINIT_Msk) | (u32Value << AWF_WBINIT_WBINIT_Pos))
+
+/**
+  * @brief      This macro is used to Write AWF data holding register value.
+  * @param[in]  u32Value is WBINIT Value. It could be 0~65535.
+  */
+#define AWF_WRITE_DAT(u32Value)     (AWF->DAT = u32Value)
 
 /**
   * @brief      Get AWF HTH Interrupt Flag
@@ -36,7 +79,6 @@ extern "C"
 
 /**
   * @brief      Clear AWF HTH Interrupt Flag
-  * @retval     None
   * @details    Write 1 to clear HTH interrupt flag.
   * \hideinitializer
   */
@@ -44,23 +86,66 @@ extern "C"
 
 /**
   * @brief      Clear AWF LTH Interrupt Flag
-  * @retval     None
   * @details    Write 1 to clear LTH interrupt flag.
   * \hideinitializer
   */
 #define AWF_CLEAR_LTH_INT_FLAG()       (AWF->STATUS |= AWF_STATUS_LTHIS_Msk)
 
+/**
+  * @brief      Enable AWF Interrupt
+  * @param[in]  u32IntMask Specify the interrupt source. Including:
+  *             - \ref AWF_CTL_HTIEN_Msk
+  *             - \ref AWF_CTL_LTIEN_Msk
+  * @details    This API is used to enable the specify AWF interrupt function.
+  */
+__STATIC_INLINE void AWF_EnableInt(uint32_t u32IntMask)
+{
+    AWF->CTL |= u32IntMask;
+}
+
+/**
+  * @brief      Disable AWF Interrupt
+  * @param[in]  u32IntMask Specify the interrupt source. Including:
+  *             - \ref AWF_CTL_HTIEN_Msk
+  *             - \ref AWF_CTL_LTIEN_Msk
+  * @details    This API is used to disable the specify AWF interrupt function.
+  */
+__STATIC_INLINE void AWF_DisableInt(uint32_t u32IntMask)
+{
+    AWF->CTL &= ~u32IntMask;
+}
+
+/**
+  * @brief      Enable Threshold Wake-up
+  * @param[in]  u32TWKMask Specify the threshold. Including:
+  *             - \ref AWF_CTL_HTWKEN_Msk
+  *             - \ref AWF_CTL_LTWKEN_Msk
+  * @details    This API is used to enable high threshold or low threshold Wake-up function.
+  */
+__STATIC_INLINE void AWF_EnableWakeup(uint32_t u32TWKMask)
+{
+    AWF->CTL |= u32TWKMask;
+}
+
+/**
+  * @brief      Disable Threshold Wake-up
+  * @param[in]  u32TWKMask Specify the threshold. Including:
+  *             - \ref AWF_CTL_HTWKEN_Msk
+  *             - \ref AWF_CTL_LTWKEN_Msk
+  * @details    This API is used to enable high threshold or low threshold Wake-up function.
+  */
+__STATIC_INLINE void AWF_DisableWakeup(uint32_t u32TWKMask)
+{
+    AWF->CTL &= ~u32TWKMask;
+}
 
 void AWF_EnableInt(uint32_t u32IntMask);
 void AWF_DisableInt(uint32_t u32IntMask);
-void AWF_EnableTWK(uint32_t u32TWKMask);
-void AWF_DisableTWK(uint32_t u32TWKMask);
-void AWF_SetAccumlationCount(uint32_t u32Count);
-void AWF_SetHTHValue(uint32_t u32Value);
-void AWF_SetLTHValue(uint32_t u32Value);
+void AWF_EnableWakeup(uint32_t u32TWKMask);
+void AWF_DisableWakeup(uint32_t u32TWKMask);
 uint32_t AWF_GetAccumlationValue(void);
-void AWF_SetWBINITValue(uint32_t u32Value);
-void AWF_SetDAT(uint32_t u32Value);
+void AWF_Open(uint32_t u32IntEn, uint32_t u32WakeupEn, uint32_t u32HTHValue, uint32_t u32LTHValue, uint32_t u32WBINITValue, uint32_t u32ACCCount);
+void AWF_Close(void);
 
 /** @} end of group AWF_EXPORTED_FUNCTIONS */
 /** @} end of group AWF_Driver */
--- Library/StdDriver/src/awf.c
+++ Library/StdDriver/src/awf.c
@@ -22,116 +22,7 @@
 */
 
 /**
-  * @brief      Enable AWF Interrupt
-  * @param[in]  u32IntFlagMask Specify the interrupt source. Including :
-  *             - \ref AWF_CTL_HTIEN_Msk
-  *             - \ref AWF_CTL_LTIEN_Msk
-  * @return     None
-  * @details    This API is used to enable the specify AWF interrupt function.
-  */
-void AWF_EnableInt(uint32_t u32IntMask)
-{
-    AWF->CTL |= u32IntMask;
-}
-
-/**
-  * @brief      Disable AWF Interrupt
-  * @param[in]  u32IntFlagMask Specify the interrupt source. Including :
-  *             - \ref AWF_CTL_HTIEN_Msk
-  *             - \ref AWF_CTL_LTIEN_Msk
-  * @return     None
-  * @details    This API is used to disable the specify AWF interrupt function.
-  */
-void AWF_DisableInt(uint32_t u32IntMask)
-{
-    AWF->CTL &= ~u32IntMask;
-}
-
-/**
-  * @brief      Enable Threshold Wake-up
-  * @param[in]  u32TWKMask Specify the threshold. Including :
-  *             - \ref AWF_CTL_HTWKEN_Msk
-  *             - \ref AWF_CTL_LTWKEN_Msk
-  * @return     None
-  * @details    This API is used to enable high threshold or low threshold Wake-up function.
-  */
-void AWF_EnableTWK(uint32_t u32TWKMask)
-{
-    AWF->CTL |= u32TWKMask;
-}
-
-/**
-  * @brief      Disable Threshold Wake-up
-  * @param[in]  u32TWKMask Specify the threshold. Including :
-  *             - \ref AWF_CTL_HTWKEN_Msk
-  *             - \ref AWF_CTL_LTWKEN_Msk
-  * @return     None
-  * @details    This API is used to enable high threshold or low threshold Wake-up function.
-  */
-void AWF_DisableTWK(uint32_t u32TWKMask)
-{
-    AWF->CTL &= ~u32TWKMask;
-}
-
-/**
-  * @brief      Set Accumlation Count
-  * @param[in]  u32Count is AccumlationCount. It could be 2~8.
-  * @return     None
-  * @details    This API is used to set accumlation count.
-  */
-void AWF_SetAccumlationCount(uint32_t u32Count)
-{
-    AWF->CTL = (AWF->CTL & ~AWF_CTL_ACUCNT_Msk) | (u32Count << AWF_CTL_ACUCNT_Pos);
-}
-
-/**
-  * @brief      Set HTH Value
-  * @param[in]  u32Value is HTH Value. It could be 0~524288.
-  * @return     None
-  * @details    This API is used to set high threshold value.
-  */
-void AWF_SetHTHValue(uint32_t u32Value)
-{
-    AWF->HTH = (AWF->HTH & ~AWF_HTH_AWFHTH_Msk) | (u32Value << AWF_HTH_AWFHTH_Pos);
-}
-
-/**
-  * @brief      Set LTH Value
-  * @param[in]  u32Value is LTH Value. It could be 0~524288.
-  * @return     None
-  * @details    This API is used to set low threshold value.
-  */
-void AWF_SetLTHValue(uint32_t u32Value)
-{
-    AWF->LTH = (AWF->LTH & ~AWF_LTH_AWFLTH_Msk) | (u32Value << AWF_LTH_AWFLTH_Pos);
-}
-
-/**
-  * @brief      Set AWF Word Buffer Initial Value
-  * @param[in]  u32Value is WBINIT Value. It could be 0~65535.
-  * @return     None
-  * @details    This API is used to set AWF word buffer initial value.
-  */
-void AWF_SetWBINITValue(uint32_t u32Value)
-{
-    AWF->WBINIT = (AWF->WBINIT & ~AWF_WBINIT_WBINIT_Msk) | (u32Value << AWF_WBINIT_WBINIT_Pos);
-}
-
-
-/**
-  * @brief      Set AWF DAT Value
-  * @param[in]  u32Value is WBINIT Value. It could be 0~65535.
-  * @return     None
-  * @details    This API is used to set AWF data holding register value.
-  */
-void AWF_SetDAT(uint32_t u32Value)
-{
-    AWF->DAT = u32Value;
-}
-
-/**
-  * @brief      Get Accumlation Value
-  * @param[in]  None
+  * @brief      Get Accumlation Value.
   * @return     Accumlation value.
   * @details    This API is used to get Accumlation value.
   *             Accumulated value will be accumulated from word0 to word[ACUCNT-1].
@@ -141,6 +32,54 @@ uint32_t AWF_GetAccumlationValue(void)
     return (AWF->ACUVAL) & AWF_ACUVAL_ACUVAL_Msk;
 }
 
+/**
+  * @brief      This API is used to enable awf function
+  * @param[in]  u32IntEn is interrupt selection.
+  *             - \ref AWF_HTINT_ENABLE
+  *             - \ref AWF_LTINT_ENABLE
+  *             - \ref AWF_BOTHINT_ENABLE
+  *             - \ref AWF_BOTHINT_DISABLE
+  * @param[in]  u32WakeupEn
+  *             - \ref AWF_HTWK_ENABLE
+  *             - \ref AWF_LTWK_ENABLE
+  *             - \ref AWF_BOTHWK_ENABLE
+  *             - \ref AWF_BOTHWK_DISABLE
+  * @param[in]  u32ACCCount is AccumlationCount. It could be 2~8.
+  * @param[in]  u32HTHValue is HTH Value. It could be 0~524288.
+  * @param[in]  u32LTHValue is LTH Value. It could be 0~524288.
+  * @param[in]  u32WBINITValue is WBINIT Value. It could be 0~65535.
+  * @details    Determine interrupt and wake-up enable or disable. \n
+  *             Set suitable threshold and word buffer initial value in this API.
+  */
+void AWF_Open(uint32_t u32IntEn, uint32_t u32WakeupEn, uint32_t u32HTHValue, uint32_t u32LTHValue, uint32_t u32WBINITValue, uint32_t u32ACCCount)
+{
+    /* Set interrupt function*/
+    AWF->CTL = ((AWF->CTL & ~(AWF_CTL_HTIEN_Msk | AWF_CTL_LTIEN_Msk | AWF_CTL_HTWKEN_Msk | AWF_CTL_LTWKEN_Msk)) | u32IntEn | u32WakeupEn);
+
+    /* Set Wake-up Function*/
+    AWF->CTL = ((AWF->CTL & ~AWF_CTL_ACUCNT_Msk) | (u32ACCCount << AWF_CTL_ACUCNT_Pos));
+
+    /* Set high threshold value*/
+    AWF_SET_HTH_VALUE(u32HTHValue);
+
+    /* Set low threshold value*/
+    AWF_SET_LTH_VALUE(u32LTHValue);
+
+    /* Set word buffer initial value*/
+    AWF_SET_WBINIT_VALUE(u32WBINITValue);
+}
+
+/**
+  * @brief      This API is used to disable awf function
+  */
+void AWF_Close(void)
+{
+    /* Clear all setting value */
+    AWF->CTL = AWF_CLOSE;
+    AWF_SET_WBINIT_VALUE(AWF_CLOSE);
+    AWF_SET_HTH_VALUE(AWF_CLOSE);
+    AWF_SET_LTH_VALUE(AWF_CLOSE);
+}
 
 /** @} end of group AWF_EXPORTED_FUNCTIONS */
 /** @} end of group AWF_Driver */
