ac01bf8312a21b8960773ec6d74139592a28b685
hllin6 <HLLIN6@nuvoton.com>
2023-07-07 13:34:42 +0800

    Update Crypto Accelerator library project code

--- Library/CryptoAccelerator/trng_api.c
+++ Library/CryptoAccelerator/trng_api.c
@@ -31,39 +31,9 @@ int mbedtls_hardware_poll( void *data, unsigned char *output, size_t len, size_t
         return -1;
 
     /* Generate the seed by TRNG */
-    if((TRNG->CTL & TRNG_CTL_READY_Msk) == 0)
-    {
-        /* Initial only when it is not ready */
-        CLK->AHBCLK0 |= CLK_AHBCLK0_CRPTCKEN_Msk;
-        CLK->APBCLK1 |= CLK_APBCLK1_TRNGCKEN_Msk;
-        CLK->APBCLK0 |= CLK_APBCLK0_RTCCKEN_Msk;
-
-        RTC->LXTCTL |= (RTC_LXTCTL_C32KSEL_Msk | RTC_LXTCTL_LIRC32KEN_Msk); //To use LIRC32K
-
-        TRNG->ACT |= TRNG_ACT_ACT_Msk;
-        /* Waiting for ready */
-        i = 0;
-        while((TRNG->CTL & TRNG_CTL_READY_Msk) == 0)
-        {
-            if(i++ > timeout)
-            {
-                /* TRNG ready timeout */
-                return -1;
-            }
-        }
-
-        TRNG->CTL = (0 << TRNG_CTL_CLKPSC_Pos);
-    }
-
-    u32Reg = TRNG->CTL;
-    for(i = 0; i < (int32_t)len; i++)
-    {
-        TRNG->CTL = TRNG_CTL_TRNGEN_Msk | u32Reg;
-        while((TRNG->CTL & TRNG_CTL_DVIF_Msk) == 0);
-        *output++ = TRNG->DATA;
-    }
-
-    *olen = len;
-
+    RNG_Open();
+		
+		 /* Get TRNG generated random number */
+		*olen =  RNG_EntropyPoll((uint32_t*)(output), len);
     return 0;
 }
