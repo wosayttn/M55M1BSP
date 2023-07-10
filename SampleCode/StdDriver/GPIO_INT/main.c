9d1e6b679ff4863f59b675257747cd60d53851e8
MS70 CCTu <CCTU@nuvoton.com>
2023-07-10 14:46:35 +0800

    added gpio sample

---
+++ SampleCode/StdDriver/GPIO_INT/main.c
@@ -0,0 +1,131 @@
+/**************************************************************************//**
+ * @file    main.c
+ * @version V1.00
+ * @brief    Show the usage of GPIO interrupt function.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ *****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+
+void GPB_IRQHandler(void)
+{
+    volatile uint32_t temp;
+
+    /* To check if PB.2 interrupt occurred */
+    if (GPIO_GET_INT_FLAG(PB, BIT2))
+    {
+        GPIO_CLR_INT_FLAG(PB, BIT2);
+        printf("PB.2 INT occurred.\n");
+    }
+    else
+    {
+        /* Un-expected interrupt. Just clear all PB interrupts */
+        temp = PB->INTSRC;
+        PB->INTSRC = temp;
+        printf("Un-expected interrupts.\n");
+    }
+}
+
+void GPC_IRQHandler(void)
+{
+    volatile uint32_t temp;
+
+    /* To check if PC.1 interrupt occurred */
+    if (GPIO_GET_INT_FLAG(PC, BIT1))
+    {
+        GPIO_CLR_INT_FLAG(PC, BIT1);
+        printf("PC.1 INT occurred.\n");
+    }
+    else
+    {
+        /* Un-expected interrupt. Just clear all PC interrupts */
+        temp = PC->INTSRC;
+        PC->INTSRC = temp;
+        printf("Un-expected interrupts.\n");
+    }
+}
+
+static void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    /* Enable PLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
+    /* Switch SCLK clock source to PLL0 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK1DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+    /* Enable UART0 module clock */
+    SetDebugUartCLK();
+    /* Enable GPIO Port B clock */
+    CLK_EnableModuleClock(GPIOB_MODULE);
+    /* Enable GPIO Port C clock */
+    CLK_EnableModuleClock(GPIOC_MODULE);
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    SetDebugUartMFP();
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int main(void)
+{
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init();
+    /* Init Debug UART to 115200-8N1 for print message */
+    InitDebugUart();
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
+    initialise_monitor_handles();
+#endif
+    printf("\n\nCPU @ %d Hz\n", SystemCoreClock);
+    printf("+------------------------------------------------+\n");
+    printf("|    GPIO PB.2 and PC.1 Interrupt Sample Code    |\n");
+    printf("+------------------------------------------------+\n\n");
+    /*-----------------------------------------------------------------------------------------------------*/
+    /* GPIO Interrupt Function Test                                                                        */
+    /*-----------------------------------------------------------------------------------------------------*/
+    printf("PB.2 and PC.1 are used to test interrupt ......\n");
+    printf("    PB.2 is rising edge trigger.\n");
+    printf("    PC.1 is falling edge trigger.\n");
+    /* Configure PB.2 as Input mode and enable interrupt by rising edge trigger */
+    GPIO_SetMode(PB, BIT2, GPIO_MODE_INPUT);
+    GPIO_EnableInt(PB, 2, GPIO_INT_RISING);
+    NVIC_EnableIRQ(GPB_IRQn);
+    /* Configure PC.1 as Quasi-bidirection mode and enable interrupt by falling edge trigger */
+    GPIO_SetMode(PC, BIT1, GPIO_MODE_QUASI);
+    GPIO_EnableInt(PC, 1, GPIO_INT_FALLING);
+    NVIC_EnableIRQ(GPC_IRQn);
+    /* Enable interrupt de-bounce function and select de-bounce sampling cycle time is 1024 clocks of LIRC clock */
+    GPIO_SET_DEBOUNCE_TIME(PB, GPIO_DBCTL_DBCLKSRC_LIRC, GPIO_DBCTL_DBCLKSEL_1024);
+    GPIO_ENABLE_DEBOUNCE(PB, BIT2);
+    GPIO_SET_DEBOUNCE_TIME(PC, GPIO_DBCTL_DBCLKSRC_LIRC, GPIO_DBCTL_DBCLKSEL_1024);
+    GPIO_ENABLE_DEBOUNCE(PC, BIT1);
+
+    /* Waiting for interrupts */
+    while (1);
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
