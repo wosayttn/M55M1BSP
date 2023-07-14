8780f023ddc71ec98fe62f4a93fdaaa1e0852c75
cytseng1 <CYTSENG1@nuvoton.com>
2023-07-14 15:34:56 +0800

    UART,Upload all sample code
    Change-Id: I50aab3e770f3e189dbd59bbf6feb949fb06e1340

---
+++ SampleCode/StdDriver/UART_TxRxFunction/main.c
@@ -0,0 +1,232 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V1.00
+ * @brief
+ *           Transmit and receive data from PC terminal through RS232 interface.
+ *
+ * @note
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include "stdio.h"
+#include "NuMicro.h"
+
+#define RXBUFSIZE   256
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global variables                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+uint8_t g_u8RecData[RXBUFSIZE]  = {0};
+
+volatile uint32_t g_u32comRbytes = 0;
+volatile uint32_t g_u32comRhead  = 0;
+volatile uint32_t g_u32comRtail  = 0;
+volatile int32_t g_bWait         = TRUE;
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Define functions prototype                                                                              */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void);
+void UART_TEST_HANDLE(void);
+void UART_FunctionTest(void);
+
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
+    extern void initialise_monitor_handles(void);
+#endif
+
+void SYS_Init(void)
+{
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+  
+    /* Enable External RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HXTEN_Msk);
+
+    /* Waiting for External RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+   /* Enable APLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
+
+    /* Switch SCLK clock source to APLL0 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+    
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+    /* Set PCLK1 divide 2 */
+    CLK_SET_PCLK1DIV(2);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+   /* Debug UART clock setting*/
+    SetDebugUartCLK();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set PB multi-function pins for Debug UART RXD and TXD */
+    SetDebugUartMFP();
+
+
+}
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+/* UART Test Sample                                                                                        */
+/* Test Item                                                                                               */
+/* It sends the received data to HyperTerminal.                                                            */
+/*---------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
+    initialise_monitor_handles();
+#endif
+
+    /* Init Debug UART for printf */
+    InitDebugUart();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* SAMPLE CODE                                                                                             */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    printf("\n\nCPU @ %d Hz\n", SystemCoreClock);
+
+    printf("\nUART Sample Program\n");
+
+    /* UART sample function */
+    UART_FunctionTest();
+
+    while(1);
+
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* ISR to handle UART Channel 0 interrupt event                                                            */
+/*---------------------------------------------------------------------------------------------------------*/
+void UART0_IRQHandler(void)
+{
+    UART_TEST_HANDLE();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* UART Callback function                                                                                  */
+/*---------------------------------------------------------------------------------------------------------*/
+void UART_TEST_HANDLE()
+{
+    uint8_t u8InChar = 0xFF;
+
+    if (UART_GET_INT_FLAG(UART0,UART_INTSTS_RDAINT_Msk))
+    {
+        printf("\nInput:");
+
+        /* Get all the input characters */
+        while(UART_IS_RX_READY(UART0))
+        {
+            /* Get the character from UART Buffer */
+            u8InChar = UART_READ(UART0);
+
+            printf("%c ", u8InChar);
+
+            if(u8InChar == '0')
+            {
+                g_bWait = FALSE;
+            }
+
+            /* Check if buffer full */
+            if(g_u32comRbytes < RXBUFSIZE)
+            {
+                /* Enqueue the character */
+                g_u8RecData[g_u32comRtail] = u8InChar;
+                g_u32comRtail = (g_u32comRtail == (RXBUFSIZE - 1)) ? 0 : (g_u32comRtail + 1);
+                g_u32comRbytes++;
+            }
+        }
+        printf("\nTransmission Test:");
+    }
+
+    if(UART_GET_INT_FLAG(UART0, UART_INTSTS_THREINT_Msk))
+    {
+        uint16_t tmp;
+        tmp = g_u32comRtail;
+        if(g_u32comRhead != tmp)
+        {
+            u8InChar = g_u8RecData[g_u32comRhead];
+            while(UART_IS_TX_FULL(UART0));  /* Wait Tx is not full to transmit data */
+            UART_WRITE(UART0, u8InChar);
+            g_u32comRhead = (g_u32comRhead == (RXBUFSIZE - 1)) ? 0 : (g_u32comRhead + 1);
+            g_u32comRbytes--;
+        }
+    }
+
+    if(UART0->FIFOSTS & (UART_FIFOSTS_BIF_Msk | UART_FIFOSTS_FEF_Msk | UART_FIFOSTS_PEF_Msk | UART_FIFOSTS_RXOVIF_Msk))
+    {
+        UART_ClearIntFlag(UART0, (UART_INTSTS_RLSINT_Msk| UART_INTSTS_BUFERRINT_Msk));
+    }
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  UART Function Test                                                                                     */
+/*---------------------------------------------------------------------------------------------------------*/
+void UART_FunctionTest()
+{
+    printf("+-----------------------------------------------------------+\n");
+    printf("|  UART Function Test                                       |\n");
+    printf("+-----------------------------------------------------------+\n");
+    printf("|  Description :                                            |\n");
+    printf("|    The sample code will print input char on terminal      |\n");
+    printf("|    Please enter any to start     (Press '0' to exit)      |\n");
+    printf("+-----------------------------------------------------------+\n");
+
+    /*
+        Using a RS232 cable to connect UART0 and PC.
+        UART0 is set to debug port. UART0 is enable RDA and RLS interrupt.
+        When inputting char to terminal screen, RDA interrupt will happen and
+        UART0 will print the received char on screen.
+    */
+
+    /* Enable UART RDA and THRE interrupt */
+    NVIC_EnableIRQ(UART0_IRQn);
+    UART_EnableInt(UART0, (UART_INTEN_RDAIEN_Msk | UART_INTEN_THREIEN_Msk));
+    while(g_bWait);
+
+    /* Disable UART RDA and THRE interrupt */
+    UART_DisableInt(UART0, (UART_INTEN_RDAIEN_Msk | UART_INTEN_THREIEN_Msk));
+    g_bWait = TRUE;
+    printf("\nUART Sample Demo End.\n");
+
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
