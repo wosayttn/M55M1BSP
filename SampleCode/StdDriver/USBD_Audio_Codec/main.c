4fb2b2d2f59251dbf540cc51fd292d175c277651
cytseng1 <CYTSENG1@nuvoton.com>
2023-08-01 12:16:58 +0800

    USBD,Upload the Sample code
    Change-Id: I0ba78e4e525f39844f76544aea6b9875de97c7b1

---
+++ SampleCode/StdDriver/USBD_Audio_Codec/main.c
@@ -0,0 +1,266 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Demonstrate how to implement a USB audio class device.
+ *           Codec is used in this sample code to play the audio data from Host.
+ *           It also supports to record data from codec to Host.
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "usbd_audio.h"
+
+#define CRYSTAL_LESS        1
+#define TRIM_INIT           (SYS_BASE+0xF40)
+
+void SYS_Init(void);
+void I2C2_Init(void);
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+   /* Enable APLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
+
+    /* Switch SCLK clock source to APLL0 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+    
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK1DIV(2);
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+
+    SystemCoreClockUpdate();
+
+    /* Enable GPA ~ GPJ peripheral clock */
+    CLK_EnableModuleClock(GPIOA_MODULE);
+    CLK_EnableModuleClock(GPIOB_MODULE);
+    CLK_EnableModuleClock(GPIOC_MODULE);
+    CLK_EnableModuleClock(GPIOD_MODULE);
+    CLK_EnableModuleClock(GPIOE_MODULE);
+    CLK_EnableModuleClock(GPIOF_MODULE);
+    CLK_EnableModuleClock(GPIOG_MODULE);
+    CLK_EnableModuleClock(GPIOH_MODULE);
+    CLK_EnableModuleClock(GPIOJ_MODULE);
+
+
+#if (!CRYSTAL_LESS)
+    /* Enable External RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HXTEN_Msk);
+
+    /* Waiting for External RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+   /* Enable APLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HXT, FREQ_180MHZ, CLK_APLL1_SELECT);   
+
+    /* Select USB clock source as PLL/2 and USB clock divider as 2 */
+    CLK_SetModuleClock(USBD_MODULE, CLK_USBSEL_USBSEL_APLL1_DIV2, CLK_USBDIV_USBDIV(2));
+#else
+    /* Enable HIRC48M clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRC48MEN_Msk);
+
+    /* Waiting for HIRC48M clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRC48MSTB_Msk);
+
+    /* Set core clock to 1800MHz */
+    CLK_SetCoreClock(FREQ_180MHZ);
+
+    /* Select USB clock source as HIRC48M and USB clock divider as 1 */
+    CLK_SetModuleClock(USBD0_MODULE, CLK_USBSEL_USBSEL_HIRC48M, CLK_USBDIV_USBDIV(1));
+#endif
+
+   /* Debug UART clock setting*/
+    SetDebugUartCLK();
+
+    /* Select USBD */
+    SYS->USBPHY = (SYS->USBPHY & ~SYS_USBPHY_USBROLE_Msk) | SYS_USBPHY_OTGPHYEN_Msk ;
+
+    /* Enable USBD module clock */
+    CLK_EnableModuleClock(USBD0_MODULE);
+
+    /* Enable TIMER0 module clock */
+    CLK_EnableModuleClock(TMR0_MODULE);
+
+    /* Select TIMER0 module clock source as HIRC */
+    CLK_SetModuleClock(TMR0_MODULE, CLK_TTMRSEL_TTMR0SEL_HIRC, 0);
+
+    /* Enable I2S0 module clock */
+    CLK_EnableModuleClock(I2S0_MODULE);
+
+    /* Enable I2C2 module clock */
+    CLK_EnableModuleClock(I2C2_MODULE);
+
+    /* Enable PDMA0 module clock */
+    CLK_EnableModuleClock(PDMA0_MODULE);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Set multi-function pins for UART0 RXD and TXD */
+    SetDebugUartMFP();
+
+    /* USBD multi-function pins for VBUS, D+, D-, and ID pins */
+    SET_USB_VBUS_PA12();
+    SET_USB_D_MINUS_PA13();
+    SET_USB_D_PLUS_PA14();
+    SET_USB_OTG_ID_PA15();
+
+    /* Set multi-function pins for I2S0 */
+    SET_I2S0_BCLK_PI6();
+    SET_I2S0_MCLK_PI7();
+    SET_I2S0_DI_PI8();
+    SET_I2S0_DO_PI9();
+    SET_I2S0_LRCK_PI10();
+
+    /* Set I2C2 multi-function pins */
+    SET_I2C2_SDA_PD0();
+    SET_I2C2_SCL_PD1();
+
+    PI->SMTEN |= GPIO_SMTEN_SMTEN6_Msk;
+    PD->SMTEN |= GPIO_SMTEN_SMTEN1_Msk;
+}
+
+/* Init I2C interface */
+void I2C2_Init(void)
+{
+    /* Open I2C2 and set clock to 100k */
+    I2C_Open(I2C2, 100000);
+
+    /* Get I2C2 Bus Clock */
+    printf("I2C clock %d Hz\n", I2C_GetBusClockFreq(I2C2));
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+#if CRYSTAL_LESS
+    uint32_t u32TrimInit;
+#endif
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART to 115200-8n1 for print message */
+    InitDebugUart();
+
+    printf("\n");
+    printf("+--------------------------------------------------------+\n");
+    printf("|              NuMicro USBD UAC Sample Code              |\n");
+    printf("+--------------------------------------------------------+\n");
+
+    /* Init I2C2 to access codec */
+    I2C2_Init();
+
+    /* Open I2S0 interface and set to slave mode, stereo channel, I2S format */
+    I2S_Open(I2S0, I2S_MODE_SLAVE, 48000, I2S_DATABIT_16, I2S_STEREO, I2S_FORMAT_I2S);
+
+    /* Select source from HIRC(12MHz) */
+    CLK_SetModuleClock(I2S0_MODULE, CLK_I2SSEL_I2S0SEL_HIRC, 0);
+
+    /* Set PD3 low to enable phone jack on NuMaker board. */
+    SYS->GPD_MFP0 &= ~(SYS_GPD_MFP0_PD3MFP_Msk);
+    GPIO_SetMode(PD, BIT3, GPIO_MODE_OUTPUT);
+    PD3 = 0;
+
+    /* Set MCLK and enable MCLK */
+    I2S_EnableMCLK(I2S0, 12000000);
+
+#if NAU8822
+    NAU8822_Setup();
+#else
+    NAU88L25_Reset();
+    /* Initialize NAU88L25 codec */
+    CLK_SysTickDelay(10000);
+    I2S0->CTL0 |= I2S_CTL0_ORDER_Msk;
+    /* Initialize NAU88L25 codec */
+    CLK_SysTickDelay(20000);
+    NAU88L25_Setup();
+#endif
+
+    /* Configure PDMA */
+    PDMA_Init();
+
+    /* Configure TIMER0 for adjusting codec's PLL */
+    TIMER_Open(TIMER0, TIMER_PERIODIC_MODE, 500);
+    TIMER_EnableInt(TIMER0);
+    NVIC_SetPriority(TIMER0_IRQn, 3);
+    NVIC_EnableIRQ(TIMER0_IRQn);
+
+    USBD_Open(&gsInfo, UAC_ClassRequest, UAC_SetInterface);
+
+    /* Endpoint configuration */
+    UAC_Init();
+    NVIC_SetPriority(USBD_IRQn, (1 << __NVIC_PRIO_BITS) - 2);
+    NVIC_EnableIRQ(USBD_IRQn);
+    USBD_Start();
+
+#if CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+        /* Start USB trim if it is not enabled. */
+        if((SYS->TCTL48M & SYS_TCTL48M_FREQSEL_Msk) != 1)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+
+                /* Re-enable crystal-less */
+                SYS->TCTL48M = 0x01;
+                SYS->TCTL48M |= SYS_TCTL48M_REFCKSEL_Msk | SYS_TCTL48M_BOUNDEN_Msk | (8 << SYS_TCTL48M_BOUNDARY_Pos);
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->TISTS48M & (SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable crystal-less */
+            SYS->TISTS48M = 0;
+
+            /* Clear error flags */
+            SYS->TISTS12M = SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk;
+
+            /* Clear SOF */
+            USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+        }
+#endif
+    }
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
+
---
+++ SampleCode/StdDriver/USBD_VCOM_And_HID_Transfer/main.c
@@ -0,0 +1,418 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Demonstrate how to implement a composite device (VCOM and HID Transfer).
+ *           Transfer data between USB device and PC through USB HID interface.
+ *           A windows tool is also included in this sample code to connect with a USB device.
+ *
+ *           Windows tool: User need to input the specific PID for the USB HID device connected to PC.
+ *                         PID format with hexadecimal.
+ *
+ *           -> PID is 0xDC00 in this sample.
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "vcom_and_hid_transfer.h"
+
+#define CRYSTAL_LESS        1
+#define TRIM_INIT           (SYS_BASE+0xF40)
+
+/*--------------------------------------------------------------------------*/
+STR_VCOM_LINE_CODING g_LineCoding = {115200, 0, 0, 8};   /* Baud rate : 115200    */
+/* Stop bit     */
+/* parity       */
+/* data bits    */
+uint16_t g_u16CtrlSignal = 0;     /* BIT0: DTR(Data Terminal Ready) , BIT1: RTS(Request To Send) */
+
+/*--------------------------------------------------------------------------*/
+#define RXBUFSIZE           512 /* RX buffer size */
+#define TXBUFSIZE           512 /* RX buffer size */
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global variables                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+/* UART0 */
+static volatile uint8_t s_au8ComRbuf[RXBUFSIZE];
+volatile uint16_t g_u16ComRbytes = 0;
+volatile uint16_t g_u16ComRhead = 0;
+volatile uint16_t g_u16ComRtail = 0;
+
+static volatile uint8_t s_au8ComTbuf[TXBUFSIZE];
+volatile uint16_t g_u16ComTbytes = 0;
+volatile uint16_t g_u16ComThead = 0;
+volatile uint16_t g_u16ComTtail = 0;
+
+static uint8_t s_au8RxBuf[64] = {0};
+uint8_t *g_pu8RxBuf = 0;
+uint32_t g_u32RxSize = 0;
+uint32_t g_u32TxSize = 0;
+
+volatile int8_t g_i8BulkOutReady = 0;
+
+void SYS_Init(void);
+void UART0_Init(void);
+void UART0_IRQHandler(void);
+void PowerDown(void);
+/*--------------------------------------------------------------------------*/
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+   /* Enable APLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
+
+    /* Switch SCLK clock source to APLL0 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+    
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK1DIV(2);
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+
+    SystemCoreClockUpdate();
+
+    /* Enable GPA ~ GPJ peripheral clock */
+    CLK_EnableModuleClock(GPIOA_MODULE);
+    CLK_EnableModuleClock(GPIOB_MODULE);
+    CLK_EnableModuleClock(GPIOC_MODULE);
+    CLK_EnableModuleClock(GPIOD_MODULE);
+    CLK_EnableModuleClock(GPIOE_MODULE);
+    CLK_EnableModuleClock(GPIOF_MODULE);
+    CLK_EnableModuleClock(GPIOG_MODULE);
+    CLK_EnableModuleClock(GPIOH_MODULE);
+    CLK_EnableModuleClock(GPIOJ_MODULE);
+
+#if (!CRYSTAL_LESS)
+    /* Enable External RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HXTEN_Msk);
+
+    /* Waiting for External RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+   /* Enable APLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HXT, FREQ_180MHZ, CLK_APLL1_SELECT);   
+
+    /* Select USB clock source as PLL/2 and USB clock divider as 2 */
+    CLK_SetModuleClock(USBD_MODULE, CLK_USBSEL_USBSEL_APLL1_DIV2, CLK_USBDIV_USBDIV(2));
+#else
+    /* Enable HIRC48M clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRC48MEN_Msk);
+
+    /* Waiting for HIRC48M clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRC48MSTB_Msk);
+
+    /* Set core clock to 1800MHz */
+    CLK_SetCoreClock(FREQ_180MHZ);
+
+    /* Select USB clock source as HIRC48M and USB clock divider as 1 */
+    CLK_SetModuleClock(USBD0_MODULE, CLK_USBSEL_USBSEL_HIRC48M, CLK_USBDIV_USBDIV(1));
+#endif
+
+   /* Debug UART clock setting*/
+    SetDebugUartCLK();
+
+    /* Select USBD */
+    SYS->USBPHY = (SYS->USBPHY & ~SYS_USBPHY_USBROLE_Msk) | SYS_USBPHY_OTGPHYEN_Msk ;
+
+    /* Enable USBD module clock */
+    CLK_EnableModuleClock(USBD0_MODULE);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Set multi-function pins for UART0 RXD and TXD */
+    SetDebugUartMFP();
+
+    /* USBD multi-function pins for VBUS, D+, D-, and ID pins */
+    SET_USB_VBUS_PA12();
+    SET_USB_D_MINUS_PA13();
+    SET_USB_D_PLUS_PA14();
+    SET_USB_OTG_ID_PA15();
+}
+
+void UART0_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset UART */
+    SYS_ResetModule(SYS_UART0RST);
+
+    /* Configure UART and set UART Baudrate */
+    UART_Open(UART0, 115200);
+
+    /* Enable Interrupt and install the call back function */
+    UART_ENABLE_INT(UART0, (UART_INTEN_RDAIEN_Msk | UART_INTEN_THREIEN_Msk | UART_INTEN_RXTOIEN_Msk));
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* UART Callback function                                                                                  */
+/*---------------------------------------------------------------------------------------------------------*/
+void UART0_IRQHandler(void)
+{
+    uint8_t u8InChar;
+    int32_t i32Size;
+    uint32_t u32IntStatus;
+
+    u32IntStatus = UART0->INTSTS;
+
+    if((u32IntStatus & UART_INTSTS_RDAIF_Msk) || (u32IntStatus & UART_INTSTS_RXTOIF_Msk))
+    {
+        /* Receiver FIFO threshold level is reached or Rx time out */
+
+        /* Get all the input characters */
+        while(!(UART0->FIFOSTS & UART_FIFOSTS_RXEMPTY_Msk))
+        {
+            /* Get the character from UART Buffer */
+            u8InChar = (uint8_t)UART0->DAT;
+
+            /* Check if buffer full */
+            if(g_u16ComRbytes < RXBUFSIZE)
+            {
+                /* Enqueue the character */
+                s_au8ComRbuf[g_u16ComRtail++] = u8InChar;
+                if(g_u16ComRtail >= RXBUFSIZE)
+                    g_u16ComRtail = 0;
+                g_u16ComRbytes++;
+            }
+            else
+            {
+                /* FIFO over run */
+            }
+        }
+    }
+
+    if(u32IntStatus & UART_INTSTS_THREIF_Msk)
+    {
+
+        if(g_u16ComTbytes && (UART0->INTEN & UART_INTEN_THREIEN_Msk))
+        {
+            /* Fill the Tx FIFO */
+            i32Size = g_u16ComTbytes;
+            if(i32Size >= UART0_FIFO_SIZE)
+            {
+                i32Size = UART0_FIFO_SIZE;
+            }
+
+            while(i32Size)
+            {
+                u8InChar = s_au8ComTbuf[g_u16ComThead++];
+                UART0->DAT = u8InChar;
+                if(g_u16ComThead >= TXBUFSIZE)
+                    g_u16ComThead = 0;
+                g_u16ComTbytes--;
+                i32Size--;
+            }
+        }
+        else
+        {
+            /* No more data, just stop Tx (Stop work) */
+            UART0->INTEN &= ~UART_INTEN_THREIEN_Msk;
+        }
+    }
+}
+
+void VCOM_TransferData(void)
+{
+    uint32_t i, u32Len;
+
+    /* Check whether USB is ready for next packet or not */
+    if(g_u32TxSize == 0)
+    {
+        /* Check whether we have new COM Rx data to send to USB or not */
+        if(g_u16ComRbytes)
+        {
+            u32Len = g_u16ComRbytes;
+            if(u32Len > EP2_MAX_PKT_SIZE)
+                u32Len = EP2_MAX_PKT_SIZE;
+
+            for(i = 0; i < u32Len; i++)
+            {
+                s_au8RxBuf[i] = s_au8ComRbuf[g_u16ComRhead++];
+                if(g_u16ComRhead >= RXBUFSIZE)
+                    g_u16ComRhead = 0;
+            }
+
+            __set_PRIMASK(1);
+            g_u16ComRbytes -= u32Len;
+            __set_PRIMASK(0);
+
+            g_u32TxSize = u32Len;
+            USBD_MemCopy((uint8_t *)(USBD_BUF_BASE + USBD_GET_EP_BUF_ADDR(EP2)), (uint8_t *)s_au8RxBuf, u32Len);
+            USBD_SET_PAYLOAD_LEN(EP2, u32Len);
+        }
+        else
+        {
+            /* Prepare a zero packet if previous packet size is EP2_MAX_PKT_SIZE and
+               no more data to send at this moment to note Host the transfer has been done */
+            u32Len = USBD_GET_PAYLOAD_LEN(EP2);
+            if(u32Len == EP2_MAX_PKT_SIZE)
+                USBD_SET_PAYLOAD_LEN(EP2, 0);
+        }
+    }
+
+    /* Process the Bulk out data when bulk out data is ready. */
+    if(g_i8BulkOutReady && (g_u32RxSize <= TXBUFSIZE - g_u16ComTbytes))
+    {
+        for(i = 0; i < g_u32RxSize; i++)
+        {
+            s_au8ComTbuf[g_u16ComTtail++] = g_pu8RxBuf[i];
+            if(g_u16ComTtail >= TXBUFSIZE)
+                g_u16ComTtail = 0;
+        }
+
+        __set_PRIMASK(1);
+        g_u16ComTbytes += g_u32RxSize;
+        __set_PRIMASK(0);
+
+        g_u32RxSize = 0;
+        g_i8BulkOutReady = 0; /* Clear bulk out ready flag */
+
+        /* Ready to get next BULK out */
+        USBD_SET_PAYLOAD_LEN(EP3, EP3_MAX_PKT_SIZE);
+    }
+
+    /* Process the software Tx FIFO */
+    if(g_u16ComTbytes)
+    {
+        /* Check if Tx is working */
+        if((UART0->INTEN & UART_INTEN_THREIEN_Msk) == 0)
+        {
+            /* Send one bytes out */
+            UART0->DAT = s_au8ComTbuf[g_u16ComThead++];
+            if(g_u16ComThead >= TXBUFSIZE)
+                g_u16ComThead = 0;
+
+            g_u16ComTbytes--;
+
+            /* Enable Tx Empty Interrupt. (Trigger first one) */
+            UART0->INTEN |= UART_INTEN_THREIEN_Msk;
+        }
+    }
+}
+
+void PowerDown(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Wakeup Enable */
+    USBD_ENABLE_INT(USBD_INTEN_WKEN_Msk);
+
+    PMC_PowerDown();
+
+    /* Clear PWR_DOWN_EN if it is not clear by itself */
+    if(PMC->PWRCTL & PMC_PWRCTL_PDEN_Msk)
+        PMC->PWRCTL ^= PMC_PWRCTL_PDEN_Msk;
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+#if CRYSTAL_LESS
+    uint32_t u32TrimInit;
+#endif
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART */
+    UART0_Init();
+
+    printf("\n");
+    printf("+--------------------------------------------------------------+\n");
+    printf("|     NuMicro USB Virtual COM and HID Transfer Sample Code     |\n");
+    printf("+--------------------------------------------------------------+\n");
+
+    USBD_Open(&gsInfo, HID_ClassRequest, NULL);
+
+    /* Endpoint configuration */
+    HID_Init();
+    USBD_Start();
+
+    NVIC_EnableIRQ(UART0_IRQn);
+
+    NVIC_EnableIRQ(USBD_IRQn);
+
+#if CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+         /* Start USB trim if it is not enabled. */
+        if((SYS->TCTL48M & SYS_TCTL48M_FREQSEL_Msk) != 1)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+
+                /* Re-enable crystal-less */
+                SYS->TCTL48M = 0x01;
+                SYS->TCTL48M |= SYS_TCTL48M_REFCKSEL_Msk | SYS_TCTL48M_BOUNDEN_Msk | (8 << SYS_TCTL48M_BOUNDARY_Pos);
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->TISTS48M & (SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable crystal-less */
+            SYS->TISTS48M = 0;
+
+            /* Clear error flags */
+            SYS->TISTS12M = SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk;
+
+            /* Clear SOF */
+            USBD_CLR_INT_FLAG(USBD_INTSTS_SOFIF_Msk);
+        }
+#endif
+
+        /* Enter power down when USB suspend */
+        if(g_u8Suspend)
+            PowerDown();
+
+        VCOM_TransferData();
+    }
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
