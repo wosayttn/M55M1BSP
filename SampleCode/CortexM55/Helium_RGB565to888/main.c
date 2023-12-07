5ea6bd1d5b50da5895e081ed1df64c8def4b64f1
hllin6 <HLLIN6@nuvoton.com>
2023-12-07 16:25:47 +0800

    Add test result and performance enhancement info

--- SampleCode/CortexM55/Helium_RGB565to888/main.c
+++ SampleCode/CortexM55/Helium_RGB565to888/main.c
@@ -10,8 +10,8 @@
 #include <string.h>
 #include "NuMicro.h"
 
-//Use Helium 
-#include <arm_mve.h> 
+//Use Helium
+#include <arm_mve.h>
 
 
 /****************************************************************************
@@ -45,7 +45,7 @@
 #define SRC_IMAGE_WIDTH			    5
 #define SRC_IMAGE_HEIGHT			  4
 
-#define DST_IMAGE_LEN				    30
+#define DST_IMAGE_LEN				    SRC_IMAGE_LEN*BYTENUMBER_RGB888
 
 #define CHECK_888RESULT
 #define SW_SCALEUP_TEST
@@ -115,16 +115,16 @@ static uint64_t Get_SysTick_Cycle_Count(void)
 void SYS_Init(void)
 {
 
-      /*---------------------------------------------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------------------------------------*/
     /* Init System Clock                                                                                       */
     /*---------------------------------------------------------------------------------------------------------*/
 
-   /* Enable Internal RC 12MHz clock */
+    /* Enable Internal RC 12MHz clock */
     CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
 
     /* Waiting for Internal RC clock ready */
     CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
-  
+
     /* Enable External RC 12MHz clock */
     CLK_EnableXtalRC(CLK_SRCCTL_HXTEN_Msk);
 
@@ -132,15 +132,15 @@ void SYS_Init(void)
     CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
 
 
-   /* Enable PLL0 200MHz clock */
-    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);    
+    /* Enable PLL0 200MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_180MHZ, CLK_APLL0_SELECT);
 
     /* Switch SCLK clock source to PLL0 and divide 1 */
     CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
 
     /* Set HCLK2 divide 2 */
     CLK_SET_HCLK2DIV(2);
-    
+
     /* Set PCLKx divide 2 */
     CLK_SET_PCLK0DIV(2);
     CLK_SET_PCLK1DIV(2);
@@ -155,8 +155,8 @@ void SYS_Init(void)
     /* Enable UART0 module clock */
     CLK_EnableModuleClock(UART0_MODULE);
 
-		 /* Debug UART clock setting*/
-     SetDebugUartCLK();
+    /* Debug UART clock setting*/
+    SetDebugUartCLK();
 
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init I/O Multi-function                                                                                 */
@@ -173,7 +173,9 @@ int32_t main(void)
 {
     uint32_t u32TimeVal;
 
-   /* Unlock protected registers */
+    uint32_t u32TimeVal_Vec;
+
+    /* Unlock protected registers */
     SYS_UnlockReg();
 
     /* Init System, IP clock and multi-function I/O. */
@@ -188,133 +190,147 @@ int32_t main(void)
     printf("|   Helium RGB565 to 888 Sample Code KEIL |\n");
     printf("+-----------------------------------------+\n");
 
-	  /* Use systick to measure inference time */
-   
-		
-    while(1)
-		{
-		    printf("\n Execute RGB565to888\n");
-	      Init_SysTick_Export();
-		    RGB565to888((uint8_t*)(image_rgb565_src), image_dst ,SRC_IMAGE_LEN);
-		    u32TimeVal = Get_SysTick_Cycle_Count();
-		    printf("\n Tick Counters are:%d!\n", u32TimeVal);
-		
-		    printf("\n Execute VRGB565to888_TP\n");
-		    Init_SysTick_Export();
-		    VRGB565to888_TP((uint8_t*)(image_rgb565_src), vimage_dst ,SRC_IMAGE_HEIGHT, SRC_IMAGE_WIDTH);
-		    u32TimeVal = Get_SysTick_Cycle_Count();
-		    printf("\n Tick Counters are:%d!\n", u32TimeVal);
-			
-			  printf("\n Test Done\n");
-			  while(1);
-		}
-		
-}
+    /* Use systick to measure inference time */
 
+
+    while(1)
+    {
+        printf("\n Execute RGB565to888\n");
+        Init_SysTick_Export();
+        RGB565to888((uint8_t*)(image_rgb565_src), image_dst,SRC_IMAGE_LEN);
+        u32TimeVal = Get_SysTick_Cycle_Count();
+        printf("\n Tick Counters are:%d!\n", u32TimeVal);
+
+        printf("\n Execute VRGB565to888_TP\n");
+        Init_SysTick_Export();
+        VRGB565to888_TP((uint8_t*)(image_rgb565_src), vimage_dst,SRC_IMAGE_HEIGHT, SRC_IMAGE_WIDTH);
+        u32TimeVal_Vec = Get_SysTick_Cycle_Count();
+        printf("\n Tick Counters are:%d!\n", u32TimeVal_Vec);
+
+        printf("\n Compare RGB565 Results \n");
+        for(int ii=0; ii<DST_IMAGE_LEN; ii++)
+        {
+            if( image_dst[ii] == vimage_dst[ii] )
+            {
+                printf("\n Match: image_dst[%d] = 0x%02x \n", ii, image_dst[ii]);
+            }
+            else
+            {
+                printf("\n Error: image_dst[%d] = 0x%02x, vimage_dst[%d] = 0x%02x \n", ii, image_dst[ii], ii, vimage_dst[ii]);
+            }
+        }
+        printf("\n RGB565 Speedup %1.2f X\n", (float)(u32TimeVal)/(float)(u32TimeVal_Vec));
+
+        printf("\n Test Done\n");
+        while(1);
+    }
+
+}
 
 
 void RGB565to888(uint8_t * src, uint8_t * dst, uint8_t len)
 {
-		 uint8_t buf888_high, buf888_low; 
-	   uint8_t dst888_r, dst888_g, dst888_b;
-	   int count = len;
-	
-	   while (count >= 0) {
-				
-					  //RGB565_IN_UINT8
-				    buf888_high = *src;
-				    buf888_low = *(src+1);				
-				
-	          //Separate RGB// 			
-				    dst888_r  = (buf888_high & 0xF8 ) ;
-			    	
-				    dst888_g  = ((buf888_high & 0x07) <<5) | ((buf888_low &0xE0) >>3) ;
-				     
-				    dst888_b  =  (buf888_low <<3) ; 
+    uint8_t buf888_high, buf888_low;
+    uint8_t dst888_r, dst888_g, dst888_b;
+    int count = len;
 
-#ifdef PADDING_RGB888_LSB			
-	          //Separate RGB with Padding// 			
-				    dst888_r  = (buf888_high & 0xF8 ) | (buf888_high >> 5) ;
-			    	
-				    dst888_g  = (((buf888_high & 0x07) <<5) | ((buf888_high & 0x07) >>1)) | ((buf888_low &0xE0) >>3) ;
-				     
-				    dst888_b  =  (buf888_low <<3) | ((buf888_low & 0x1F)>> 2); 
-#endif			 
-				    //Store to Memory
-				    *(dst++) = dst888_r; *(dst++) = dst888_g; *(dst++) = dst888_b;
-			
-						src += BYTENUMBER_RGB565;
-						count --;
-			}
+    while (count >= 0) {
+
+        //RGB565_IN_UINT8
+        buf888_high = *src;
+        buf888_low = *(src+1);
+
+        //Separate RGB//
+        dst888_r  = (buf888_high & 0xF8 ) ;
+
+        dst888_g  = ((buf888_high & 0x07) <<5) | ((buf888_low &0xE0) >>3) ;
+
+        dst888_b  =  (buf888_low <<3) ;
+
+#ifdef PADDING_RGB888_LSB
+        //Separate RGB with Padding//
+        dst888_r  = (buf888_high & 0xF8 ) | (buf888_high >> 5) ;
+
+        dst888_g  = (((buf888_high & 0x07) <<5) | ((buf888_high & 0x07) >>1)) | ((buf888_low &0xE0) >>3) ;
+
+        dst888_b  =  (buf888_low <<3) | ((buf888_low & 0x1F)>> 2);
+#endif
+        //Store to Memory
+        *(dst++) = dst888_r;
+        *(dst++) = dst888_g;
+        *(dst++) = dst888_b;
+
+        src += BYTENUMBER_RGB565;
+        count --;
+    }
 }
 
 
 void VRGB565to888_TP(uint8_t * src, uint8_t * dst, uint16_t size_h, uint16_t size_w)
 {
-		 int8_t count;
-	  uint32_t TimeVal;
-		 
-	   uint8x16_t vsrc_8_16_lo;
-		 uint8x16_t vsrc_8_16_hi;
-		 uint8x16_t vdst_8_16_r;
-		 uint8x16_t vdst_8_16_g;
-		 uint8x16_t vdst_8_16_b;
-		 uint8x16_t offset_8_16;
-	   uint8x16_t mask_8_16_r;
+    int8_t count;
+    uint8x16_t vsrc_8_16_lo;
+    uint8x16_t vsrc_8_16_hi;
+    uint8x16_t vdst_8_16_r;
+    uint8x16_t vdst_8_16_g;
+    uint8x16_t vdst_8_16_b;
+    uint8x16_t offset_8_16;
 
-     count = size_h*size_w;
-	
-	   mask_8_16_r = vdupq_n_u8(0xF8);
+    count = size_h*size_w;
+    while (count >0)
+    {
+        //RGB565_IN_UINT8
 
-		 while (count >0) 
-		 {
-			//RGB565_IN_UINT8
-			
-			//Include tail predication to handle improper tailing.
-			mve_pred16_t p = vctp8q(count);
-			 
-			offset_8_16 = vcreateq_u8(OFFSET_565LOWBYTE_LOW, OFFSET_565LOWBYTE_HIGH);
-		
-			vsrc_8_16_lo = vldrbq_gather_offset_z_u8(src, offset_8_16, p);
-			 
-		  offset_8_16 = vcreateq_u8(OFFSET_565HIGHBYTE_LOW, OFFSET_565HIGHBYTE_HIGH);
-				
-			vsrc_8_16_hi = vldrbq_gather_offset_z_u8(src, offset_8_16, p);
+        //Include tail predication to handle improper tailing.
+        mve_pred16_t p = vctp8q(count);
 
-			//Separate RGB// 
-			 vdst_8_16_r  =  vandq_u8(vsrc_8_16_hi, mask_8_16_r);
-			  
-		   //[2022-07-12] :Note vrshrq vs vshrq. vrshrq results in round. That is 0x07 rounds to 0x08.
+        offset_8_16 = vcreateq_u8(OFFSET_565LOWBYTE_LOW, OFFSET_565LOWBYTE_HIGH);
 
-			 vdst_8_16_g =  vorrq_u8(vrshlq(vsrc_8_16_hi, 5), vrshlq(vshrq(vsrc_8_16_lo, 5), 2));
-				
+        vsrc_8_16_lo = vldrbq_gather_offset_z_u8(src, offset_8_16, p);
 
-			 vdst_8_16_b =  vrshlq(vsrc_8_16_lo, 3);
+        offset_8_16 = vcreateq_u8(OFFSET_565HIGHBYTE_LOW, OFFSET_565HIGHBYTE_HIGH);
 
-			  //Scatter Store//
-			 offset_8_16 = vcreateq_u8(OFFSET_R_LOW, OFFSET_R_HIGH);
+        vsrc_8_16_hi = vldrbq_gather_offset_z_u8(src, offset_8_16, p);
 
-			 vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_r ,p);
+        //Separate RGB//
+        vdst_8_16_r  =  vrshlq(vshrq(vsrc_8_16_hi, 3), 3);
 
-			 offset_8_16 = vcreateq_u8(OFFSET_G_LOW, OFFSET_G_HIGH);
+        //[2022-07-12] :Note vrshrq vs vshrq. vrshrq results in round. That is 0x07 rounds to 0x08.
+        vdst_8_16_g =  vorrq_u8(vrshlq(vsrc_8_16_hi, 5), vrshlq(vshrq(vsrc_8_16_lo, 5), 2));
 
-			 vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_g ,p);
+        vdst_8_16_b =  vrshlq(vsrc_8_16_lo, 3);
 
-			 offset_8_16 = vcreateq_u8(OFFSET_B_LOW, OFFSET_B_HIGH);
+#ifdef PADDING_RGB888_LSB
+        //Padding LSB with MSB
 
-			 vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_b ,p);
+        vdst_8_16_r  =  vorrq_u8(vrshlq(vshrq(vsrc_8_16_hi, 3), 3), vshrq(vsrc_8_16_hi, 5));
 
+        vdst_8_16_g =  vorrq_u8(vorrq_u8(vrshlq(vsrc_8_16_hi, 5), vrshlq(vshrq(vsrc_8_16_lo, 5), 2)),  vshrq(vrshlq(vsrc_8_16_hi, 5), 6));
 
-			//Update data source and destination pointer
-			
-			//Source: shift one RGB566 pixel size(2 byte)		
-			src+= BYTENUMBER_RGB565*PIXELS_CONVERT_EACHLOOP;
-			
-			//Destination: shift one RGB888 pixel size(3 byte)
-			dst+= BYTENUMBER_RGB888*PIXELS_CONVERT_EACHLOOP;
-			
-			count-= PIXELS_CONVERT_EACHLOOP;
-			
-		}
+        vdst_8_16_b =  vorrq_u8(vrshlq(vsrc_8_16_lo, 3), vshrq(vrshlq(vsrc_8_16_lo, 3), 5));
+#endif
+        //Scatter Store//
+        offset_8_16 = vcreateq_u8(OFFSET_R_LOW, OFFSET_R_HIGH);
+
+        vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_r,p);
+
+        offset_8_16 = vcreateq_u8(OFFSET_G_LOW, OFFSET_G_HIGH);
+
+        vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_g,p);
+
+        offset_8_16 = vcreateq_u8(OFFSET_B_LOW, OFFSET_B_HIGH);
+
+        vstrbq_scatter_offset_p(dst, offset_8_16, vdst_8_16_b,p);
+
+        //Update data source and destination pointer
+
+        //Source: shift one RGB566 pixel size(2 byte)
+        src+= BYTENUMBER_RGB565*PIXELS_CONVERT_EACHLOOP;
+
+        //Destination: shift one RGB888 pixel size(3 byte)
+        dst+= BYTENUMBER_RGB888*PIXELS_CONVERT_EACHLOOP;
+
+        count-= PIXELS_CONVERT_EACHLOOP;
+
+    }
 }
-
