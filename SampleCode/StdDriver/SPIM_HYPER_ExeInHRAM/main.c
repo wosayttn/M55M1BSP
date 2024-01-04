245d0d06569e8280624e04d3a4938c97c8190a65
CCWang20 <ccwang20@nuvoton.com>
2024-01-04 13:43:07 +0800

    Fixed I2S/LPSPI/PSIO/QSPI/SDH/SPI/SPIM/USPI GCC project not use newlib-nano issue.

--- SampleCode/StdDriver/SPIM_HYPER_ExeInHRAM/main.c
+++ SampleCode/StdDriver/SPIM_HYPER_ExeInHRAM/main.c
@@ -35,128 +35,6 @@ void Reset_Handler_PreInit(void)
     CLK_SET_PCLK2DIV(2);
     CLK_SET_PCLK3DIV(2);
     CLK_SET_PCLK4DIV(2);
-    /* Enable SPIM module clock */
-    CLK_EnableModuleClock(SPIM1_MODULE);
-
-    /* Enable GPIO Module clock */
-    CLK_EnableModuleClock(GPIOD_MODULE);
-    CLK_EnableModuleClock(GPIOH_MODULE);
-    CLK_EnableModuleClock(GPIOJ_MODULE);
-
-    /* Init SPIM multi-function pins */
-    SET_SPIM1_CLKN_PH12();
-    SET_SPIM1_CLK_PH13();
-    SET_SPIM1_D2_PJ4();
-    SET_SPIM1_D3_PJ3();
-    SET_SPIM1_D4_PH15();
-    SET_SPIM1_D5_PD7();
-    SET_SPIM1_D6_PD6();
-    SET_SPIM1_D7_PD5();
-    SET_SPIM1_MISO_PJ5();
-    SET_SPIM1_MOSI_PJ6();
-    SET_SPIM1_RESETN_PJ2();
-    SET_SPIM1_RWDS_PH14();
-    SET_SPIM1_SS_PJ7();
-
-    PD->SMTEN |= (GPIO_SMTEN_SMTEN5_Msk |
-                  GPIO_SMTEN_SMTEN6_Msk |
-                  GPIO_SMTEN_SMTEN7_Msk);
-    PH->SMTEN |= (GPIO_SMTEN_SMTEN12_Msk |
-                  GPIO_SMTEN_SMTEN13_Msk |
-                  GPIO_SMTEN_SMTEN14_Msk |
-                  GPIO_SMTEN_SMTEN15_Msk);
-    PJ->SMTEN |= (GPIO_SMTEN_SMTEN2_Msk |
-                  GPIO_SMTEN_SMTEN3_Msk |
-                  GPIO_SMTEN_SMTEN4_Msk |
-                  GPIO_SMTEN_SMTEN5_Msk |
-                  GPIO_SMTEN_SMTEN6_Msk |
-                  GPIO_SMTEN_SMTEN7_Msk);
-
-    /* Set SPIM I/O pins as high slew rate up to 80 MHz. */
-    GPIO_SetSlewCtl(PD, BIT5, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PD, BIT6, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PD, BIT7, GPIO_SLEWCTL_HIGH);
-
-    GPIO_SetSlewCtl(PH, BIT12, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PH, BIT13, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PH, BIT14, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PH, BIT15, GPIO_SLEWCTL_HIGH);
-
-    GPIO_SetSlewCtl(PJ, BIT2, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PJ, BIT3, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PJ, BIT4, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PJ, BIT5, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PJ, BIT6, GPIO_SLEWCTL_HIGH);
-    GPIO_SetSlewCtl(PJ, BIT7, GPIO_SLEWCTL_HIGH);
-
-    HyperRAM_Init(SPIM1);
-    SPIM_HYPER_EnterDirectMapMode(SPIM1);
-}
-
-//------------------------------------------------------------------------------
-void SPIM_SetDMMAddrNonCacheable(void)
-{
-    uint32_t u32DMMAddr = SPIM_GetDMMAddress(SPIM_PORT);
-
-    /* Disable D-Cache */
-    SCB_DisableDCache();
-
-    /* Configure MPU memory attribute */
-    /*
-     * Attribute 0
-     * Memory Type = Normal
-     * Attribute   = Outer Non-cacheable, Inner Non-cacheable
-     */
-    ARM_MPU_SetMemAttr(0UL, ARM_MPU_ATTR(ARM_MPU_ATTR_NON_CACHEABLE, ARM_MPU_ATTR_NON_CACHEABLE));
-
-    /* Configure MPU memory regions */
-    ARM_MPU_SetRegion(0UL,                                                          /* Region 0 */
-                      ARM_MPU_RBAR((uint32_t)u32DMMAddr, ARM_MPU_SH_NON, 0, 0, 0),  /* Non-shareable, read/write, privileged, non-executable */
-                      ARM_MPU_RLAR((uint32_t)u32DMMAddr + 0x10000, 0)               /* Use Attr 0 */
-                     );
-    /* Enable MPU */
-    ARM_MPU_Enable(MPU_CTRL_PRIVDEFENA_Msk);
-
-    /* Enable D-Cache */
-    SCB_EnableDCache();
-}
-
-void SYS_Init(void)
-{
-    /* Enable Internal RC 12MHz clock */
-    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
-
-    /* Waiting for Internal RC clock ready */
-    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
-
-    /* Enable PLL0 180MHz clock */
-    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_160MHZ, CLK_APLL0_SELECT);
-
-    /* Switch SCLK clock source to PLL0 and divide 1 */
-    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
-
-    /* Set HCLK2 divide 2 */
-    CLK_SET_HCLK2DIV(2);
-
-    /* Set PCLKx divide 2 */
-    CLK_SET_PCLK0DIV(2);
-    CLK_SET_PCLK1DIV(2);
-    CLK_SET_PCLK2DIV(2);
-    CLK_SET_PCLK3DIV(2);
-    CLK_SET_PCLK4DIV(2);
-
-    /* Update System Core Clock */
-    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
-    SystemCoreClockUpdate();
-
-    /* Enable UART0 module clock */
-    SetDebugUartCLK();
-
-    /*---------------------------------------------------------------------------------------------------------*/
-    /* Init I/O Multi-function                                                                                 */
-    /*---------------------------------------------------------------------------------------------------------*/
-    SetDebugUartMFP();
-#if 0
 
     if (SPIM_PORT == SPIM0)
     {
@@ -258,7 +136,73 @@ void SYS_Init(void)
         GPIO_SetSlewCtl(PJ, BIT7, GPIO_SLEWCTL_HIGH);
     }
 
-#endif
+    HyperRAM_Init(SPIM_PORT);
+    SPIM_HYPER_EnterDirectMapMode(SPIM_PORT);
+}
+
+//------------------------------------------------------------------------------
+void SPIM_SetDMMAddrNonCacheable(void)
+{
+    uint32_t u32DMMAddr = SPIM_GetDMMAddress(SPIM_PORT);
+
+    /* Disable D-Cache */
+    SCB_DisableDCache();
+
+    /* Configure MPU memory attribute */
+    /*
+     * Attribute 0
+     * Memory Type = Normal
+     * Attribute   = Outer Non-cacheable, Inner Non-cacheable
+     */
+    ARM_MPU_SetMemAttr(0UL, ARM_MPU_ATTR(ARM_MPU_ATTR_NON_CACHEABLE, ARM_MPU_ATTR_NON_CACHEABLE));
+
+    /* Configure MPU memory regions */
+    ARM_MPU_SetRegion(0UL,                                                          /* Region 0 */
+                      ARM_MPU_RBAR((uint32_t)u32DMMAddr, ARM_MPU_SH_NON, 0, 0, 0),  /* Non-shareable, read/write, privileged, non-executable */
+                      ARM_MPU_RLAR((uint32_t)u32DMMAddr + 0x10000, 0)               /* Use Attr 0 */
+                     );
+    /* Enable MPU */
+    ARM_MPU_Enable(MPU_CTRL_PRIVDEFENA_Msk);
+
+    /* Enable D-Cache */
+    SCB_EnableDCache();
+}
+
+void SYS_Init(void)
+{
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_SRCCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Enable PLL0 180MHz clock */
+    CLK_EnableAPLL(CLK_APLLCTL_APLLSRC_HIRC, FREQ_160MHZ, CLK_APLL0_SELECT);
+
+    /* Switch SCLK clock source to PLL0 and divide 1 */
+    CLK_SetSCLK(CLK_SCLKSEL_SCLKSEL_APLL0);
+
+    /* Set HCLK2 divide 2 */
+    CLK_SET_HCLK2DIV(2);
+
+    /* Set PCLKx divide 2 */
+    CLK_SET_PCLK0DIV(2);
+    CLK_SET_PCLK1DIV(2);
+    CLK_SET_PCLK2DIV(2);
+    CLK_SET_PCLK3DIV(2);
+    CLK_SET_PCLK4DIV(2);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /* Enable UART0 module clock */
+    SetDebugUartCLK();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    SetDebugUartMFP();
 }
 
 int main()
@@ -272,15 +216,9 @@ int main()
     /* Init Debug UART to 115200-8N1 for print message */
     InitDebugUart();
 
-    //SPIM_SetDMMAddrNonCacheable();
-
     printf("+-------------------------------------------------------+\n");
     printf("|       SPIM DMM mode running program on HyperRAM       |\n");
     printf("+-------------------------------------------------------+\n");
-
-    //    HyperRAM_Init(SPIM_PORT);
-
-    //    SPIM_HYPER_EnterDirectMapMode(SPIM_PORT);
 
     while (1)
     {
