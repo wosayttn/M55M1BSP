5e5e260d5d4f0fc0ab960adce8dce1097091e62d
MS70 MCChang2 <MCCHANG2@nuvoton.com>
2024-01-23 19:43:52 +0800

    Remove CACHE_OFF
    Change-Id: I29417cc66b44d1182913d0c743be6802818ea114

--- SampleCode/StdDriver/EMAC_lwiperf/main.c
+++ SampleCode/StdDriver/EMAC_lwiperf/main.c
@@ -6,21 +6,7 @@
  * @copyright SPDX-License-Identifier: Apache-2.0
  * @copyright (C) 2020 Nuvoton Technology Corp. All rights reserved.
 *****************************************************************************/
-/*
- * This is a template project for M55M1 series MCU.
- * Users can create their own application based on this project.
- *
- * This template uses internal RC as APLL0 clock source and UART to print messages.
- * Users may need to do extra system configuration according to their system design.
- *
- * I/D-Cache
- *   I/D-Cache are enabled by default for better performance,
- *   users can define NVT_ICACHE_OFF/NVT_DCACHE_OFF in project setting to disable cache.
- * Debug UART
- *   system_M55M1.c has three weak functions as below to configure debug UART port.
- *     SetDebugUartMFP, SetDebugUartCLK and InitDebugUart
- *   Users can re-implement these functions according to system design.
- */
+
 #include <stdio.h>
 #include <string.h>
 #include "NuMicro.h"
