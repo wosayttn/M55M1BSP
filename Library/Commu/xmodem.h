8b25062c3897f16f956a55398205067e1cb935f9
klchiu0 <klchiu0@nuvoton.com>
2024-01-29 14:00:41 +0800

    Fix naming Add compiler option for XMD_UART_PORT

--- Library/Commu/xmodem.h
+++ Library/Commu/xmodem.h
@@ -8,7 +8,9 @@
 #ifndef _XMODEM_H
 #define _XMODEM_H
 
-#define XMD_UART_PORT           DEBUG_PORT
+#ifndef XMD_UART_PORT
+    #define XMD_UART_PORT       DEBUG_PORT
+#endif
 
 /* Xmodem Standard Commands */
 #define XMD_SOH                 0x01
@@ -31,6 +33,6 @@
 #define MAXRETRANS              25
 
 int32_t XmodemRecv(uint32_t u32DestAddr);
-int32_t XmodemSend(uint8_t *pu8Src, int32_t srcsz);
+int32_t XmodemSend(uint8_t *pu8Src, int32_t i32SrcSize);
 
 #endif
